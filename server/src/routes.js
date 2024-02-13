af414c1ed02727f1b397194eec8ec6501871a6ef
DanielAGS <danielalmeidagsilva@gmail.com>
2024-02-13 12:48:56 -0300

    Back-end Finalizado

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 
 // Importações de controladores para encapsulamento de lógica
-const OngController = require('./controllers/OngController')
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 // Criação e definição do roteador e rotas da aplicação
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
